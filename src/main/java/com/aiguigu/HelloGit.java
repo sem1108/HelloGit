83acdd870bb90354f32d3302ff955f48ffe8ab7f
root <3553209547@qq.com>
2024-07-21 10:35:00 +0800

    更新1

--- src/main/java/com/aiguigu/HelloGit.java
+++ src/main/java/com/aiguigu/HelloGit.java
@@ -3,5 +3,7 @@ package com.aiguigu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        //在git仓库更新直接提交
+        System.out.println("更新1");
     }
 }
