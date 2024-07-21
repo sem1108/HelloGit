b97fd5e611ace194fdd274960960cfa63136cfbe
root <3553209547@qq.com>
2024-07-21 11:22:36 +0800

    切换分支

--- src/main/java/com/aiguigu/HelloGit.java
+++ src/main/java/com/aiguigu/HelloGit.java
@@ -7,6 +7,8 @@ public class HelloGit {
         System.out.println("更新1");
         System.out.println("更新2");
         System.out.println("更新3");
-
+        System.out.println("切换分支");
+        //变更版本：在项目上右键选择git里的历史记录，复制哈希值（修改号）在git里找到重置HEAD，选择为HEAD，下面粘贴复制的修改号
+        //创建分支：在git里选择分支，在本地分支创建分支，签出切换分支
     }
 }
